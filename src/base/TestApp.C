67f4c3d51c06dcd75a1d8c6b8a06d176b2ca0dee
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-08-11 15:47:38 -0600

    Update to new constructors

--- src/base/TestApp.C
+++ src/base/TestApp.C
@@ -13,10 +13,9 @@ InputParameters validParams<TestApp>()
   return params;
 }
 
-TestApp::TestApp(const std::string & name, InputParameters parameters) :
-    MooseApp(name, parameters)
+TestApp::TestApp(InputParameters parameters) :
+    MooseApp(parameters)
 {
-  srand(processor_id());
 
   Moose::registerObjects(_factory);
   ModulesApp::registerObjects(_factory);
